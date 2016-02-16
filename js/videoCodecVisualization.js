b95ad17a58771e560bece22f65a86b5359666ea5
John D <deblanc.john@gmail.com>
2016-02-15 23:44:18 -0600

    Placeholder for donut graph.

--- js/videoCodecVisualization.js
+++ js/videoCodecVisualization.js
@@ -13,6 +13,7 @@ function getCodecDataset() {
     });
 
     logCodecDataSet(codecDataSet);
+    visualizeDataSet(codecDataSet);
   });
 }
 
@@ -23,5 +24,18 @@ function logCodecDataSet(codecDataSet) {
     $('#codecDataSummary').append('<p>' + codec + ': ' + codecDataSet[codec] + '</p>');
   }
 };
+
+function visualizeDataSet(codecDataSet) {
+  var width = 960;
+  var height = 500;
+  var radius = Math.min(width, height) / 2;
+
+  var svg = d3.select('.graph')
+    .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+    .append('g')
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')' );
+}
 
 getCodecDataset();
