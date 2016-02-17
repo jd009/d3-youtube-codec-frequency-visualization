ea95e4d11bde2022197e2bc799ced6f1219a8942
John D <deblanc.john@gmail.com>
2016-02-16 19:44:48 -0600

    Added donut graph visualization.

--- js/videoCodecVisualization.js
+++ js/videoCodecVisualization.js
@@ -12,20 +12,21 @@ function getCodecDataset() {
       }
     });
 
-    logCodecDataSet(codecDataSet);
-    visualizeDataSet(codecDataSet);
+    var codecInfoArray = convertToCodecInfoArray(codecDataSet);
+    logCodecDataSet(codecInfoArray);
+    visualizeDataSet(codecInfoArray);
   });
 }
 
-function logCodecDataSet(codecDataSet) {
+function logCodecDataSet(codecInfoArray) {
   $('#codecDataSummary').empty();
   $('#codecDataSummary').append('<p>Codec: Frequency Count</p>');
-  for(var codec in codecDataSet) {
-    $('#codecDataSummary').append('<p>' + codec + ': ' + codecDataSet[codec] + '</p>');
-  }
-};
+  codecInfoArray.forEach(function(codecInfo) {
+    $('#codecDataSummary').append('<p>' + codecInfo.name + ': ' + codecInfo.frequencyCount + '</p>');
+  });
+}
 
-function visualizeDataSet(codecDataSet) {
+function visualizeDataSet(codecInfoArray) {
   var width = 960;
   var height = 500;
   var radius = Math.min(width, height) / 2;
@@ -36,6 +37,47 @@ function visualizeDataSet(codecDataSet) {
       .attr('height', height)
     .append('g')
       .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')' );
+
+  var color = d3.scale.ordinal()
+    .range(["#98abc5", "#7b6888", "#a05d56", "#d0743c", "#ff8c00"]);
+
+  var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+  var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) {
+      return d.frequencyCount; });
+
+  var g = svg.selectAll('.arc')
+    .data(pie(codecInfoArray))
+    .enter().append('g')
+      .attr('class', 'arc');
+
+  g.append('path')
+    .attr('d', arc)
+    .style('fill', function(d) { return color(d.data.frequencyCount); });
+
+  g.append('text')
+    .attr('transform', function(d) { return 'translate(' + arc.centroid(d) + ')'; })
+    .attr('dy', '.35em')
+    .text(function(d) { return d.data.name; });
+}
+
+function convertToCodecInfoArray(codecDataSet) {
+  var codecInfoArray = [];
+  for(var codec in codecDataSet) {
+    var codecInfo = new CodecInfo(codec, codecDataSet[codec]);
+    codecInfoArray.push(codecInfo);
+  }
+
+  return codecInfoArray;
+}
+
+function CodecInfo(name, frequencyCount) {
+  this.name = name;
+  this.frequencyCount = frequencyCount;
 }
 
 getCodecDataset();
